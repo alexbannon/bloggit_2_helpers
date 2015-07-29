2d7476902d1eaccecfd0ede426e5b2576716b68c
Alex Bannon <alexbannon@gmail.com>
2015-07-28 20:37:08 -0400

    CRUD functionality for posts and users, Create and Delete functionality for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,23 @@
 class CommentsController < ApplicationController
-  def index
-    @comments = Comment.all
+  def new
+    @comment = Comment.new
+    @users = User.all
+    @posts = Post.all
+  end
+  def create
+    @comment = Comment.create!(comment_params)
+    redirect_to post_path(@comment.post_id)
+  end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@comment.post_id)
+  end
+
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id, :post_id, :comment_content)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,36 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def new
+    @post = Post.new
+    @users = User.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def create
+    @post = Post.create!(post_params)
+    redirect_to post_path(@post)
+  end
+  def edit
+    @post = Post.find(params[:id])
+    @users = User.all
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :post_content, :user_id)
+  end
 end
